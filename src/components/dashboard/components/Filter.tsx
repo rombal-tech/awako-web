b0f76c98394e7cedc0dbc610847a1e8afd274ed0
Egor Khokhlov <egor.unknown05@mail.ru>
2023-04-14 20:29:13 +0500

    Реализованы два поля Creation Date и Last Modified, создано оформаление списка схем

--- src/components/dashboard/components/Filter.tsx
+++ src/components/dashboard/components/Filter.tsx
@@ -6,29 +6,27 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import MenuItem from '@mui/material/MenuItem';
 
 const Filter = () => (
-	<div className="filter-inside">
-		<PopupState variant="popover" popupId="demo-popup-menu">
-			{(popupState) => (
-				<>
-					<Button
-						size="large"
-						variant="outlined"
-						color="inherit"
-						className="filter-button"
-						{...bindTrigger(popupState)}
-						startIcon={<KeyboardArrowDownIcon />}
-					>
-						SORT BY
-					</Button>
-					<Menu {...bindMenu(popupState)}>
-						<MenuItem onClick={popupState.close}>Alphabetically</MenuItem>
-						<MenuItem onClick={popupState.close}>Last created</MenuItem>
-						<MenuItem onClick={popupState.close}>Last modified</MenuItem>
-					</Menu>
-				</>
-			)}
-		</PopupState>
-	</div>
+	<PopupState variant="popover" popupId="demo-popup-menu">
+		{(popupState) => (
+			<>
+				<Button
+					size="large"
+					variant="outlined"
+					color="inherit"
+					className="filter-button"
+					{...bindTrigger(popupState)}
+					startIcon={<KeyboardArrowDownIcon />}
+				>
+					SORT BY
+				</Button>
+				<Menu {...bindMenu(popupState)}>
+					<MenuItem onClick={popupState.close}>Alphabetically</MenuItem>
+					<MenuItem onClick={popupState.close}>Last created</MenuItem>
+					<MenuItem onClick={popupState.close}>Last modified</MenuItem>
+				</Menu>
+			</>
+		)}
+	</PopupState>
 );
 
 export default Filter;
