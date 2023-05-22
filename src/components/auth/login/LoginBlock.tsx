190e326c8b4ac2605a974bf475d26334bb6bd03f
execaus <execaus@gmail.com>
2023-05-22 18:30:01 +0500

    Добавляет переход на страницу регистрации

--- src/components/auth/login/LoginBlock.tsx
+++ src/components/auth/login/LoginBlock.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 import EmailInput from '../../ui/inputs/EmailInput';
 import PasswordInput from '../../ui/inputs/PasswordInput';
 import SecondaryButton from '../../ui/SecondaryButton';
@@ -13,24 +14,27 @@ const onLoginClick = () => {
 	console.log('login click!');
 };
 
-const onRegistrationClick = () => {
-	console.log('registration click!');
+const onRegistrationClick = (navigate: NavigateFunction) => {
+	navigate('/registration');
 };
 
-const LoginBlock = () => (
-	<ContentPrimaryFrame className="login-page__login-block">
-		<h1>Log in</h1>
-		<EmailInput className="login-frame__input login-frame__input_email" placeholder="Email" />
-		<PasswordInput className="login-frame__input login-frame__input_password" placeholder="Password" />
-		<div className="login-frame__forgot-password-box">
-			<SecondaryButton content="Forgot password ?" onClick={onForgotPasswordClick} />
-		</div>
-		<PrimaryButton className="login-frame__login-button" content="Login" onClick={onLoginClick} />
-		<div className="login-frame__registration-box">
-			<p>Not registered yet?</p>
-			<SecondaryButton content="Registration" onClick={onRegistrationClick} />
-		</div>
-	</ContentPrimaryFrame>
-);
+const LoginBlock = () => {
+	const navigate = useNavigate();
+	return (
+		<ContentPrimaryFrame className="login-page__login-block">
+			<h1>Log in</h1>
+			<EmailInput className="login-frame__input login-frame__input_email" placeholder="Email" />
+			<PasswordInput className="login-frame__input login-frame__input_password" placeholder="Password" />
+			<div className="login-frame__forgot-password-box">
+				<SecondaryButton content="Forgot password ?" onClick={onForgotPasswordClick} />
+			</div>
+			<PrimaryButton className="login-frame__login-button" content="Login" onClick={onLoginClick} />
+			<div className="login-frame__registration-box">
+				<p>Not registered yet?</p>
+				<SecondaryButton content="Registration" onClick={() => onRegistrationClick(navigate)} />
+			</div>
+		</ContentPrimaryFrame>
+	);
+};
 
 export default LoginBlock;
